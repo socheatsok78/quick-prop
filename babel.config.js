1ed2bb93f966d473b41f023dee6616b54e6e1bd1
Socheat Sok <alex@socheat.net>
2020-05-23 18:16:32 +0700

    build(packaging): install @babel/plugin-transform-flow-strip-types

--- babel.config.js
+++ babel.config.js
@@ -5,6 +5,7 @@ module.exports = {
         ],
     ],
     plugins: [
+        "@babel/plugin-transform-flow-strip-types",
         "@babel/plugin-proposal-class-properties",
         "@babel/plugin-proposal-private-methods"
     ]
