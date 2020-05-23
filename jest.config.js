7d64eb35a5eb67895ed5596fc6ada1e190baf6ac
Socheat Sok <alex@socheat.net>
2020-05-23 13:42:12 +0700

    init project

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  moduleFileExtensions: [
+    'js',
+  ],
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest'
+  },
+  transformIgnorePatterns: [
+    '/node_modules/'
+  ],
+  moduleNameMapper: {
+    '^@/(.*)\\?inline$': '<rootDir>/src/$1',
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  testMatch: [
+    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
+  ]
+}
