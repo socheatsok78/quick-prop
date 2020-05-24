6fb68519ee01833b207644387d0fef532b340c6c
Socheat Sok <alex@socheat.net>
2020-05-24 12:22:12 +0700

    feat(core): add flow type check to src/util/debug.js

--- src/util/debug.js
+++ src/util/debug.js
@@ -1,9 +1,10 @@
+// @flow
 import { noop } from './shared'
 
 export let warn = noop
 
 if (process.env.NODE_ENV !== 'production') {
-    warn = (msg) => {
+    warn = (msg: string) => {
         console.error(`[QuickProp warn]: ${msg}`)
     }
 }
